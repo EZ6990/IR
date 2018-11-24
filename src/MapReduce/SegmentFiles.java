c60ddfc298d7f5086290da7146fb88eaaa7d82fc
koyfdan <@hi>
2018-11-24 22:02:20 +0200

    segmentFiles and co

--- src/MapReduce/SegmentFiles.java
+++ src/MapReduce/SegmentFiles.java
@@ -1,14 +1,50 @@
 package MapReduce;
 
 import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class SegmentFiles {
+public class SegmentFiles implements Runnable {
 
-
-    private HashMap<String,PostingDocument> map;
+    private boolean bStop;
+    private HashMap<String, TermDocumentInfo> map;
     private int mapCounter;
+    private ConcurrentLinkedQueue<HashMap<String, TermDocumentInfo>> TDIqueue;
+    private HashMap<String, Queue<TermDocumentInfo>> postFile;
+    private int numOfDocs;
+    private SegmentWriter writer;
+
+    public SegmentFiles(ConcurrentLinkedQueue<HashMap<String, TermDocumentInfo>> TDIqueue) {
+        this.TDIqueue = TDIqueue;
+        mapCounter = 0;
+        numOfDocs = 18;
+        postFile = new HashMap<>();
+        bStop=false;
+        writer=new SegmentWriter();
+    }
+
+    @Override
+    public void run() {
+        while ((map = TDIqueue.poll()) != null && !bStop) {
+            for (String s : map.keySet()
+
+                    ) {
+                if (postFile.containsKey(s))
+                    postFile.get(s).add(map.get(s));
+                else {
+                    postFile.put(s, new PriorityQueue<TermDocumentInfo>());
+                }
+
+                if (++mapCounter == numOfDocs) {
+                    writer.write(postFile);
+                    mapCounter=0;
+                    postFile=new HashMap<>();
+                }
 
 
+            }
 
-
+        }
+    }
 }
---
+++ src/MapReduce/SegmentWriter.java
@@ -0,0 +1,12 @@
+package MapReduce;
+
+import java.util.HashMap;
+import java.util.Queue;
+
+public class SegmentWriter {
+
+
+
+    public void write(HashMap<String, Queue<TermDocumentInfo>> postFile) {
+    }
+}
