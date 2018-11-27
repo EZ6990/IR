faf142931d33ac318329f22ed7d4449d03dca2b1
tal_malul <tal.malul4@gmail.com>
2018-11-27 17:12:31 +0200

    added the country reading

--- src/MapReduce/CityTDI.java
+++ src/MapReduce/CityTDI.java
@@ -1,5 +1,6 @@
 package MapReduce;
 
+import Main.CountryInfo;
 import Main.Term;
 
 public class CityTDI extends TermDocumentInfo {
@@ -15,6 +16,14 @@ public class CityTDI extends TermDocumentInfo {
         this.currency = currency;
         this.termLocation = "";
         this.population = population;
+    }
+
+    public CityTDI(Term term, int documentID, CountryInfo info) {
+        super(term, documentID);
+        this.country = info.getCountryName();
+        this.currency = info.getCurrency();
+        this.population = info.getPopulation();
+        this.termLocation = "";
     }
 
     public void addLocation(int location) {
--- src/MapReduce/Parsers/CountryParser.java
+++ src/MapReduce/Parsers/CountryParser.java
@@ -1,9 +1,13 @@
 package MapReduce.Parsers;
 
 import IO.HTTPWebRequest;
+import Main.CountryInfo;
+import Main.Term;
+import MapReduce.CityTDI;
 import MapReduce.TermDocumentInfo;
 import TextOperations.TokenizedDocument;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 public class CountryParser extends AbstractParser {
@@ -14,6 +18,20 @@ public class CountryParser extends AbstractParser {
 
     @Override
     public void manipulate() {
+        int docSize = getTxtSize();
+        int i = 0;
+        while (i < docSize){
+            CountryInfo country = null;
+            if ((country = getCountryInfo((get(i).getWord()))) != null);
+            new CityTDI(new Term(country.getCapitalName()))
+            }
+    }
 
+    private CountryInfo getCountryInfo(String CapitalName){
+        try {
+            return new CountryInfo(CapitalName);
+        } catch (IOException e) {
+            return null;
+        }
     }
 }
