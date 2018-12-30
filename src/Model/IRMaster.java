d2618b589c8d23adbba2f1e901f3d7b688e1cb8f
talmalu <@adf>
2018-12-30 21:07:30 +0200

    changes

--- src/Model/IRMaster.java
+++ src/Model/IRMaster.java
@@ -25,15 +25,15 @@ public class IRMaster {
     private ConcurrentLinkedQueue<File> files_queue;
     private ConcurrentLinkedQueue<Document> document_queue;
     private ConcurrentLinkedQueue<TokenizedDocument> tokenized_queue;
-    private ConcurrentLinkedQueue<HashMap<String,AbstractTermDocumentInfo>> tdi_queue;
+    private ConcurrentLinkedQueue<HashMap<String, AbstractTermDocumentInfo>> tdi_queue;
 
-    private Thread [] doc_readers;
-    private Thread [] text_operators;
-    private Thread [] parsers;
+    private Thread[] doc_readers;
+    private Thread[] text_operators;
+    private Thread[] parsers;
 
-    private Runnable [] runnable_doc_readers;
-    private Runnable [] runnable_text_operators;
-    private Runnable [] runnable_parsers;
+    private Runnable[] runnable_doc_readers;
+    private Runnable[] runnable_text_operators;
+    private Runnable[] runnable_parsers;
 
 
     private Semaphore document_reader_producer;
@@ -45,13 +45,13 @@ public class IRMaster {
 
     private StopWords stopWords;
     private Stemmer stemmer;
-
+    private List<QueryResult> queryResultList;
 
     public IRMaster(Stemmer stemmer) {
         this.files_queue = new ConcurrentLinkedQueue<File>();
         this.document_queue = new ConcurrentLinkedQueue<Document>();
         this.tokenized_queue = new ConcurrentLinkedQueue<TokenizedDocument>();
-        this.tdi_queue = new ConcurrentLinkedQueue<HashMap<String,AbstractTermDocumentInfo>>();
+        this.tdi_queue = new ConcurrentLinkedQueue<HashMap<String, AbstractTermDocumentInfo>>();
 
         this.doc_readers = new Thread[1];
         this.runnable_doc_readers = new Runnable[1];
@@ -62,13 +62,13 @@ public class IRMaster {
         this.parsers = new Thread[1];
         this.runnable_parsers = new Runnable[1];
 
-        this.document_reader_producer = new Semaphore(5000,true);
-        this.text_operation_consumer =  new Semaphore(0,true);
-        this.text_operation_producer = new Semaphore(10000,true);
-        this.master_parser_consumer = new Semaphore(0,true);
-        this.master_parser_producer = new Semaphore(30000,true);
-        this.segment_file_consumer = new Semaphore(0,true);
-
+        this.document_reader_producer = new Semaphore(5000, true);
+        this.text_operation_consumer = new Semaphore(0, true);
+        this.text_operation_producer = new Semaphore(10000, true);
+        this.master_parser_consumer = new Semaphore(0, true);
+        this.master_parser_producer = new Semaphore(30000, true);
+        this.segment_file_consumer = new Semaphore(0, true);
+        this.queryResultList = new ArrayList<>();
 
         if (!(DataProvider.getInstance().getQueriesLocation() == null)) {
             this.files_queue.add(new File(DataProvider.getInstance().getQueriesLocation()));
@@ -83,10 +83,9 @@ public class IRMaster {
         long start = System.currentTimeMillis();
         System.out.println("Start : " + LocalTime.now());
 
-        if (query != null)
-        {
+        if (query != null) {
             this.document_reader_producer.acquire();
-            this.document_queue.add(new Document("","","","",query,""));
+            this.document_queue.add(new Document("", "", "", "", query, ""));
             this.text_operation_consumer.release();
             //ReaderFinished();
         }
@@ -101,65 +100,72 @@ public class IRMaster {
         WaitParsers();
 
 
-        IRanker ranker = new BM25Ranker(0.2,(float)0.5,getAvdl(),DataProvider.getInstance().getDocumentIndexer().size());
+        IRanker ranker = new BM25Ranker(0.2, (float) 0.5, getAvdl(), DataProvider.getInstance().getDocumentIndexer().size());
         SimpleSearcher searcher = new SimpleSearcher();
         HashMap<AbstractTermDocumentInfo, SegmentFile> queryToRank;
         while (!this.tdi_queue.isEmpty()) {
             HashMap<String, AbstractTermDocumentInfo> thisQuery = this.tdi_queue.poll();
-            DataProvider.getInstance().addRankedDocumentsForQuery(thisQuery.values().toArray()[0].toString(),ranker.returnRankedDocs(searcher.search(thisQuery,getCorpusCityFilterDocuments(Filter))));
+            DataProvider.getInstance().addRankedDocumentsForQuery(thisQuery.values().toArray()[0].toString(), ranker.returnRankedDocs(searcher.search(thisQuery, getCorpusCityFilterDocuments(Filter))));
         }
 
 
-
-
     }
-    private void StartReaders(){
-        for (int i = 0; i < this.doc_readers.length ; i++) {
-            this.doc_readers[i] = new Thread((this.runnable_doc_readers[i] = new DocumentReader(this.files_queue,this.document_queue,this.document_reader_producer,this.text_operation_consumer, XMLReader.Type.QUERY)));
+
+    private void StartReaders() {
+        for (int i = 0; i < this.doc_readers.length; i++) {
+            this.doc_readers[i] = new Thread((this.runnable_doc_readers[i] = new DocumentReader(this.files_queue, this.document_queue, this.document_reader_producer, this.text_operation_consumer, XMLReader.Type.QUERY)));
             this.doc_readers[i].start();
         }
     }
-    private void StartTextOperators(){
-        for (int i = 0; i < this.text_operators.length ; i++) {
-            this.text_operators[i] = new Thread((this.runnable_text_operators[i] = new TextOperations(this.document_queue,this.tokenized_queue,new Tokenize(),this.stopWords,this.document_reader_producer,this.text_operation_consumer,this.text_operation_producer,this.master_parser_consumer)));
+
+    private void StartTextOperators() {
+        for (int i = 0; i < this.text_operators.length; i++) {
+            this.text_operators[i] = new Thread((this.runnable_text_operators[i] = new TextOperations(this.document_queue, this.tokenized_queue, new Tokenize(), this.stopWords, this.document_reader_producer, this.text_operation_consumer, this.text_operation_producer, this.master_parser_consumer)));
             this.text_operators[i].start();
         }
     }
+
     private void StartParsers() {
         IFilter ignore = (this.stopWords.intersection(new RulesWords()));
-        for (int i = 0; i < this.parsers.length ; i++) {
-            this.parsers[i] = new Thread((this.runnable_parsers[i] = new MasterParser(this.tokenized_queue,this.tdi_queue,this.text_operation_producer,this.master_parser_consumer,this.master_parser_producer,this.segment_file_consumer,this.stemmer,ignore)));
+        for (int i = 0; i < this.parsers.length; i++) {
+            this.parsers[i] = new Thread((this.runnable_parsers[i] = new MasterParser(this.tokenized_queue, this.tdi_queue, this.text_operation_producer, this.master_parser_consumer, this.master_parser_producer, this.segment_file_consumer, this.stemmer, ignore)));
             this.parsers[i].start();
         }
     }
+
     private void WaitReaders() throws InterruptedException {
-        for (int i = 0; i < this.doc_readers.length ; i++) {
+        for (int i = 0; i < this.doc_readers.length; i++) {
             this.doc_readers[i].join();
         }
         ReaderFinished();
     }
+
     private void WaitTextOperators() throws InterruptedException {
-        for (int i = 0; i < this.text_operators.length ; i++) {
+        for (int i = 0; i < this.text_operators.length; i++) {
             this.text_operators[i].join();
         }
         TextOperatorsFinished();
     }
+
     private void WaitParsers() throws InterruptedException {
-        for (int i = 0; i < this.parsers.length ; i++) {
+        for (int i = 0; i < this.parsers.length; i++) {
             this.parsers[i].join();
         }
         ParsersFinished();
     }
-    private void ReaderFinished(){
-        this.document_queue.add(new Document("DannyAndTalSendTheirRegardsYouFucker","","","","",""));
+
+    private void ReaderFinished() {
+        this.document_queue.add(new Document("DannyAndTalSendTheirRegardsYouFucker", "", "", "", "", ""));
         this.text_operation_consumer.release();
         System.out.println("Finished Read Files : " + LocalTime.now());
     }
+
     private void TextOperatorsFinished() {
-          this.tokenized_queue.add(new TokenizedDocument("DannyAndTalSendTheirRegardsYouFucker","",null,null,null));
+        this.tokenized_queue.add(new TokenizedDocument("DannyAndTalSendTheirRegardsYouFucker", "", null, null, null));
         this.master_parser_consumer.release();
         System.out.println("Finished Text Operations : " + LocalTime.now());
     }
+
     private void ParsersFinished() {
 //        for (int i = 0; i < this.runnable_segments.length ; i++) {
 //            ((SegmentFiles)this.runnable_segments[i]).Stop();
@@ -171,32 +177,32 @@ public class IRMaster {
         System.out.println("Finished Parsing : " + LocalTime.now());
     }
 
-    private List<Info> getCorpusCityFilterDocuments(List<String> cities){
+    private List<Info> getCorpusCityFilterDocuments(List<String> cities) {
 
-        List <Info> docs = null;
-        CitySegmentFile cityPost = new CitySegmentFile(DataProvider.getInstance().getPostLocation() + "\\" + DataProvider.getInstance().getPrefixPost() + "cityPost.post",null,new SegmentCityReader());
+        List<Info> docs = null;
+        CitySegmentFile cityPost = new CitySegmentFile(DataProvider.getInstance().getPostLocation() + "\\" + DataProvider.getInstance().getPrefixPost() + "cityPost.post", null, new SegmentCityReader());
         if (cities != null && cities.size() > 0) {
             docs = new ArrayList<Info>();
             CityIndexer cityIndexer = DataProvider.getInstance().getCityIndexer();
             for (String city : cities)
-                cityPost.read(new Term(city,this.stemmer),Integer.parseInt(cityIndexer.getValue(city).split(" ")[0]));
-            for (List<Info> lstInfo : cityPost.getData().values()){
+                cityPost.read(new Term(city, this.stemmer), Integer.parseInt(cityIndexer.getValue(city).split(" ")[0]));
+            for (List<Info> lstInfo : cityPost.getData().values()) {
                 docs.addAll(lstInfo);
             }
         }
         return docs;
     }
 
-    private int getAvdl(){
+    private int getAvdl() {
 
         Iterator it = DataProvider.getInstance().getDocumentIndexer().iterator();
         int total = 0;
-        while(it.hasNext()){
-            Map.Entry pair = (Map.Entry)it.next();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry) it.next();
             total += Integer.parseInt(pair.getValue().toString().split(" ")[2]);
             //it.remove();
         }
 
-        return (total/DataProvider.getInstance().getDocumentIndexer().size());
+        return (total / DataProvider.getInstance().getDocumentIndexer().size());
     }
 }
